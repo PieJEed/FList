49ae6e778cc1c9f437d69ce5dbfaeee2f2f86270
The Pack <57849314+PieJEed@users.noreply.github.com>
2024-11-02 21:16:43 +0800

    Update vuepress.config.ts

--- vuepress.config.ts
+++ vuepress.config.ts
@@ -21,9 +21,9 @@ export default defineUserConfig({
   lang: 'zh-CN',
   public: `./public`,
   // 网站标题，标题颜色可在 src/client/css/main.css 中修改
-  title: 'PieJEShare',
+  title: 'LiShare',
   // 网站的简介，有助于搜索引擎收录
-  description: 'PieJEShare-不止软件分享',
+  description: 'LiShare - 不止分享好的开源仓库',
   // 页面 <head> 标签内添加的额外标签。 不要修改/logo.png可以替换掉这个文件，删除logo.png会导致构建出错。
   head: [['link', { rel: 'icon', href: '/logo.png' }]],
   // 页面预加载，所有其它页面所需的文件都会被预拉取。这对于小型站点来说是十分有帮助的，因为它会大大提升页面切换的速度。但是在你的网站有很多页面时不建议你这么做。
@@ -32,16 +32,154 @@ export default defineUserConfig({
   // 主题配置 FileList 是 vuepress 的一个主题，文件展示的功能全部由这个主题提供。
   theme: FileList([
     {
-      mountPath: "/",
-      analysis: githubReposAnalysis({
-        user: "PieJEed",
-        repository: "FileList",
+        mountPath: "/",
+      analysis: githubReleasesFilesAnalysis({ user: "PieJEed", repository: "FList" }),
+      // 下载代理配置,支持多个平台，参考:https://jjaw.cn/2024/8/3/flist-config-porxy/
+      // 这个是为了解决github的国内下载慢的问题，和跨域问题，建议配置，不然pdf，txt，md等文件因为跨域无法预览
+      // 如果你使用的不是 cloudflare Pages 部署需要删掉这一行，因为如果不是cloudflare Pages部署，这个代理是无法正常工作的
+      downProxy: cloudflarePagesDownProxy(),
+    },
+    // {
+    //   mountPath: "/",
+    //   // 这里使用 fileUrlTreeAnalysis 文件放到对应的文件路径中
+    //   analysis: fileUrlTreeAnalysis({
+    //     "/test2/文件树-测试视频1.mp4": "https://github.com/jianjianai/FList/releases/download/root/test.video.2.1080p.webm",
+    //     "/文件树测试/文件树-测试视频1.mp4": "https://github.com/jianjianai/FList/releases/download/root/test.video.2.1080p.webm",
+    //     "/文件树-测试视频1.mp4": "https://github.com/jianjianai/FList/releases/download/root/test.video.2.1080p.webm"
+    //   }),
+    //   downProxy: cloudflarePagesDownProxy(),//如果文件树地址下载比较慢，也可以配置代理
+    // },
+    // {
+    //   mountPath: "/huggingface测试",
+    //   analysis: huggingFaceDatasetsAnalysis({
+    //     userName: "Open-Orca",
+    //     datasetsName: "OpenOrca",
+    //     branchName: "main",
+    //     path: "/",
+    //     //最大深度,如果文件夹有很多层最大递归解析多少层，默认10
+    //     maxDeep: 3
+    //   }),
+    // },
+    // {
+    //   mountPath: "/gitee测试/发行版",
+    //   analysis: giteeReleasesFilesAnalysis({
+    //     user: "jja8",
+    //     repository: "flist-test",
+    //     direction: "desc"
+    //   })
+    // },
+    // {
+    //   mountPath: "/gitee测试/仓库",
+    //   analysis: giteeReposAnalysis({
+    //     user: "jja8",
+    //     repository: "flist-test"
+    //   }),
+    // },
+    // {
+    //   mountPath: "/ProgrammingVTuberLogos",
+    //   analysis: githubReposAnalysis({
+    //     user: "Aikoyori",
+    //     repository: "ProgrammingVTuberLogos",
+    //   }),
+    //   downProxy: cloudflarePagesDownProxy()
+    // },
+    // ... 可以配置多个挂载路径和仓库，以此类推
+    
+    //Windows优化
+    //路径格式：Windows/优化/
+        {
+      // 挂载路径
+      mountPath: "Windows/优化/“此电脑”第三方图标删除",
+      // 文件解析器，这里使用githubReleasesFilesAnalysis,可以解析github的release文件
+      analysis: githubReleasesFilesAnalysis({
+        // 仓库所有者的用户名
+        user: "Return-Log",
+        // 仓库所有者的仓库名
+        repository: "Drive-Icon-Manager"
+      }),
+      downProxy: cloudflarePagesDownProxy(),
+    },
+        {
+      // 挂载路径
+      mountPath: "Windows/优化/ShowDesktopOneMonitor｜解决 WIN+E 会关闭多显示器的窗口",
+      // 文件解析器，这里使用githubReleasesFilesAnalysis,可以解析github的release文件
+      analysis: githubReleasesFilesAnalysis({
+        // 仓库所有者的用户名
+        user: "ruzrobert",
+        // 仓库所有者的仓库名
+        repository: "ShowDesktopOneMonitor"
       }),
     },
-    {
-      mountPath:"/",
-      analysis:githubReleasesFilesAnalysis({user:"PieJEed", repository:"FileList"})
+    
+    //Windows美化
+    //路径格式：Windows/美化/
+        {
+      // 挂载路径
+      mountPath: "Windows/美化/WinDynamicDesktop・MAC功能 壁纸随时间改变",
+      // 文件解析器，这里使用githubReleasesFilesAnalysis,可以解析github的release文件
+      analysis: githubReleasesFilesAnalysis({
+        // 仓库所有者的用户名
+        user: "t1m0thyj",
+        // 仓库所有者的仓库名
+        repository: "WinDynamicDesktop"
+      }),
     },
-    // ... 可以配置多个挂载路径和仓库，以此类推
+    
+    //分割
+        {
+      // 挂载路径
+      mountPath: "Windows/电子书阅读/Koodo-reader・UI蛮棒,
+      // 文件解析器，这里使用githubReleasesFilesAnalysis,可以解析github的release文件
+      analysis: githubReleasesFilesAnalysis({
+        // 仓库所有者的用户名
+        user: "koodo-reader",
+        // 仓库所有者的仓库名
+        repository: "koodo-reader"
+      }),
+    },
+        {
+      // 挂载路径
+      mountPath: "Windows/优化/“此电脑”第三方图标删除",
+      // 文件解析器，这里使用githubReleasesFilesAnalysis,可以解析github的release文件
+      analysis: githubReleasesFilesAnalysis({
+        // 仓库所有者的用户名
+        user: "Return-Log",
+        // 仓库所有者的仓库名
+        repository: "Drive-Icon-Manager"
+      }),
+    },
+        {
+      // 挂载路径
+      mountPath: "/Windows优化/“此电脑”第三方图标删除",
+      // 文件解析器，这里使用githubReleasesFilesAnalysis,可以解析github的release文件
+      analysis: githubReleasesFilesAnalysis({
+        // 仓库所有者的用户名
+        user: "Return-Log",
+        // 仓库所有者的仓库名
+        repository: "Drive-Icon-Manager"
+      }),
+    },
+        {
+      // 挂载路径
+      mountPath: "/Windows优化/“此电脑”第三方图标删除",
+      // 文件解析器，这里使用githubReleasesFilesAnalysis,可以解析github的release文件
+      analysis: githubReleasesFilesAnalysis({
+        // 仓库所有者的用户名
+        user: "Return-Log",
+        // 仓库所有者的仓库名
+        repository: "Drive-Icon-Manager"
+      }),
+      downProxy: cloudflarePagesDownProxy(),
+    },
+        {
+      // 挂载路径
+      mountPath: "/Windows优化/“此电脑”第三方图标删除",
+      // 这里使用githubReleasesFilesAnalysis,解析github的release文件
+      analysis: githubReleasesFilesAnalysis({
+        user: "Return-Log",
+        repository: "Drive-Icon-Manager"
+      }),
+      downProxy: cloudflarePagesDownProxy(),
+    },
   ])
 })
